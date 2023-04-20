2d832d2570e515130a5663074346c8aa20326062
حـمود³¹³ <120424663+Mohmmadhacking@users.noreply.github.com>
2023-04-20 04:11:29 +0300

    Update Password.py
    You can generate a random password that is difficult to crack

--- Password.py
+++ Password.py
@@ -1,2 +1,15 @@
 # password-python-
-Random  generate password 
+
+import random
+
+lower = "abcdefghijklmnopwrstuvwxyz"
+upper = "ABCEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+simbols = "!@#$%^&*()_+-=[]{}|;':\",./<>?"
+
+all = lower + upper + number + simbols #combinazioni
+length = 10 #lunghezza password
+
+while True:
+    password = "".join(random.choices(all, k=length))
+    print(password)
